8277891efd9d496520ab5f8d2fcfc52e60b306ba
Shiro Han <shan99business@gmail.com>
2020-05-21 20:17:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,11 @@ end
 def using_flatten(array)
   array.flatten
 end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, index)
+  array.delete_at(index)
+end
